b582aadf1cbbbb10824ebdc43a8390ae49c5c66a
Lukas Martinelli <me@lukasmartinelli.ch>
2014-11-20 21:25:42 +0100

    Big refactoring

---
+++ github.js
@@ -0,0 +1,50 @@
+var request = require('request');
+
+module.exports = function(accessToken) {
+    var options = {
+        url: 'https://api.github.com/events',
+        headers: {
+            'User-Agent': 'OGHHR',
+            'Authorization': 'token ' + accessToken,
+            'If-None-Match': '""',
+        }
+    };
+    var log = {
+        ratelimit: {
+            limit: 5000,
+            remaining: 0,
+            reset: 0
+        },
+        events: 0,
+        requests: 0,
+    };
+    var parseInfo = function(headers, events) {
+            options.headers['If-None-Match'] = headers['etag'];
+            log.ratelimit.limit = headers['x-ratelimit-limit'];
+            log.ratelimit.remaining = headers['x-ratelimit-remaining'];
+            log.ratelimit.reset = headers['x-ratelimit-reset'] * 1000;
+
+            log.events += events.length;
+    };
+    return {
+        getEvents : function(callback) {
+            log.requests += 1;
+            request(options, function(error, response, body) {
+                if(error) {
+                    console.log(err);
+                }
+                if(response.statusCode == 304) {
+                    callback([]);
+                }
+                if(response.statusCode === 200) {
+                    var events = JSON.parse(body);
+                    parseInfo(response.headers, events);
+                    callback(events);
+                }
+            });
+        },
+        getInfo: function() {
+            return log;
+        }
+    };
+};
--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-var github = require('octonode');
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
@@ -10,87 +9,83 @@ var accessToken = args[0];
 var pollInterval = args[1] || 1000;
 var port = args[2] || 3000;
 
+var client = require('./github')(accessToken);
 var dns = require('dns');
 var os = require('os');
 
-var ratelimit = 0;
-var remaining = 0;
-var reset = 0;
-var requestsSent = 0;
-var eventsReceived = 0;
-var duplicates = 0;
-var client = github.client(accessToken);
-
-var getEvents = function(callback) {
-    requestsSent += 1;
-    client.get('/events', {}, function(err, status, body, headers) {
-        if(!err) {
-            ratelimit = headers['x-ratelimit-limit'];
-            remaining = headers['x-ratelimit-remaining'];
-            reset = headers['x-ratelimit-reset'] * 1000;
-            eventsReceived += body.length;
-        }
-        callback(err, body);
-    });
-};
-
-var compareEvents = function(event, other) {
-    return event.id === other.id;
-};
-
-var relayInfo = function() {
-    io.emit("info", {
-        "ratelimit": ratelimit,
-        "remaining": remaining,
-        "poll_interval": pollInterval,
-        "reset": reset,
-        "duplicates": duplicates,
-    });
-
-};
-
-var previousIds = {};
-var removeDuplicates = function(event) {
-    if(event.id in previousIds) {
-        duplicates += 1;
-        previousIds[event.id] += 1;
-        return false;
-    } else {
-        previousIds[event.id] = 1;
-        return true;
-    }
-};
-
-var relayEvent = function(event) {
-    console.log([event.id, event.type,
-                 event.created_at, previousIds[event.id]
-                ].join("\t"));
-    io.emit(event.type.toLowerCase(), event);
-};
-
-var getInfo = function(req, res) {
+var connectionInfo = function(callback) {
     dns.lookup(os.hostname(), function (err, addr, fam) {
-        res.json({
+        callback({
             "address": addr,
             "hostname": os.hostname(),
             "port": port,
-            "ratelimit": ratelimit,
-            "remaining": remaining,
+        });
+    });
+};
+
+var relayInfo = function() {
+    io.emit("info", client.getInfo());
+};
+
+var relayEvent = function(event) {
+    var timestamp = new Date(event.created_at).toLocaleTimeString();
+    console.log([event.id, event.type, timestamp].join("\t"));
+    io.emit(event.type.toLowerCase(), event);
+};
+
+var duplicates = 0;
+var getInfo = function(req, res) {
+    connectionInfo(function(connInfo) {
+        res.json({
+            'connection': connInfo,
+            'client': client.getInfo(),
             "poll_interval": pollInterval,
-            "requests_sent": requestsSent,
-            "events_received": eventsReceived,
-            "reset": reset,
             "duplicates": duplicates,
         });
     });
 };
+
 app.use(express.static(__dirname + '/public'));
 app.get('/info', getInfo);
 http.listen(port);
 
+var compareEvents = function(a, b) {
+    if(a.id < b.id) { return -1; }
+    if(a.id > b.id) { return 1; }
+    return 0;
+}
+
+var findAndCountDuplicates = function(a, b) {
+    if(a.id === b.id) {
+        duplicates += 1;
+        return true;
+    }
+    return false;
+};
+
+var ids = {};
+var findAllDuplicatesNonScalableAndMemoryLeaky = function(event) {
+    if(event.id in ids) {
+        duplicates += 1;
+        ids[event.id] += 1;
+        return false;
+    } else {
+        ids[event.id] = 1;
+        return true;
+    }
+};
+
+var filterStream = function(stream) {
+    return stream
+         .bufferWithTime(2 * pollInterval)
+         .map(function(events) { return events.sort(compareEvents); })
+         .flatMap(Bacon.fromArray)
+         .skipDuplicates(findAndCountDuplicates)
+};
+
 Bacon.interval(1000).onValue(relayInfo);
 Bacon.interval(pollInterval)
-     .flatMap(function() { return Bacon.fromNodeCallback(getEvents); })
+     .flatMap(function() { return Bacon.fromCallback(client.getEvents); })
      .flatMap(Bacon.fromArray)
-     .filter(removeDuplicates)
+     .filter(findAllDuplicatesNonScalableAndMemoryLeaky)
      .onValue(relayEvent);
